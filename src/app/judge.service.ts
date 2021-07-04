1acc001a2709f1a73429bf779b8be16ce244f6db
SandipChakraborty <catch2sandy007@gmail.com>
2021-07-04 12:50:41 +0530

    Refactored and added all languages support.

--- src/app/judge.service.ts
+++ src/app/judge.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SourceCode } from './SourceCode';
 import { Observable } from 'rxjs';
 import { Result } from './Result';
+import { Language } from './Language';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,22 @@ import { Result } from './Result';
 export class JudgeService {
 
   PORT:number = 2358;
+  BASE_URI = `http://localhost:${this.PORT}`;
 
   constructor(private http: HttpClient) { }
 
   postCode(sourceCode: SourceCode): Observable<any> {
-    const uri = `http://localhost:${this.PORT}/submissions/?base64_encoded=true`;
+    const uri = `${this.BASE_URI}/submissions/?base64_encoded=true`;
     return this.http.post(uri, sourceCode);
   }
 
   checkCodeStatus(token: string): Observable<Result> {
-    const uri = `http://localhost:${this.PORT}/submissions/${token}?base64_encoded=true`;
+    const uri = `${this.BASE_URI}/submissions/${token}?base64_encoded=true`;
     return this.http.get<Result>(uri);
+  }
+
+  getAllLanguages() : Observable<Array<Language>> {
+    const uri = `${this.BASE_URI}/languages/all`;
+    return this.http.get<Array<Language>>(uri);
   }
 }
